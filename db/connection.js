d4438219fce7b25531523eb42176a9ed7132724b
ayaneey <ayan.h18@hotmail.com>
2022-09-27 14:18:52 +0100

    set up database connection

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username
+    user: "root",
+    password: "",
+    database: "employee_tracker",
+  },
+  console.log(`Connected to the employee_tracker database.`)
+);
+
+module.exports = db;
